9e501a10315b01232b172a506840f4c75bc3163b
KamilRustamovich <zhavdedovkamil@protonmail.com>
2022-03-12 17:09:29 +0600

    add: typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { BlogModule } from '@app/blog/blog.module';
 import { UserModule } from '@app/user/user.module';
+import { typeOrmConfig } from '@app/configs/typeorm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [BlogModule, UserModule],
+  imports: [
+    BlogModule, 
+    UserModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeOrmConfig),
+  ],
   controllers: [],
   providers: [],
 })
