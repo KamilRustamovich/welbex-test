e2ff1b4275ad508ea44a6e9196c0e442b46e8ab3
KamilRustamovich <zhavdedovkamil@protonmail.com>
2022-03-12 16:09:43 +0600

    add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BlogModule } from './blog/blog.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [BlogModule],
+  imports: [BlogModule, UserModule],
   controllers: [],
   providers: [],
 })
